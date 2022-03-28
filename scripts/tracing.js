5819b96abc51a518c570ce31acf34a0e90db10f9
Michael Beemer <michael.beemer@dynatrace.com>
2022-03-28 12:13:09 -0400

    Resolved build dependency issues

--- scripts/tracing.js
+++ scripts/tracing.js
@@ -1,29 +1,34 @@
-'use strict'
+'use strict';
 
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
 
 registerInstrumentations({
-  instrumentations: [
-    getNodeAutoInstrumentations()
-  ]
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
 const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { NodeTracerProvider } = require('@opentelemetry/node')
-const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin')
-const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-
+const {
+  SemanticResourceAttributes,
+} = require('@opentelemetry/semantic-conventions');
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
+const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "api-service",
-  })
+    [SemanticResourceAttributes.SERVICE_NAME]: 'api-service',
+  }),
 });
-  
-provider.addSpanProcessor(new BatchSpanProcessor(new ZipkinExporter({
-  url: 'http://localhost:9411/api/v2/spans',
-})))
 
-provider.register()
+provider.addSpanProcessor(
+  new BatchSpanProcessor(
+    new ZipkinExporter({
+      url: 'http://localhost:9411/api/v2/spans',
+    })
+  )
+);
+
+provider.register();
