cb9c4dae1ad8e29525525b1aca9e6fd39c750bb9
Michael Beemer <michael.beemer@dynatrace.com>
2022-06-03 11:14:28 -0400

    Add hex after hook comment for clarity

--- packages/app/src/app/hex-color/hex-color.service.ts
+++ packages/app/src/app/hex-color/hex-color.service.ts
@@ -24,6 +24,10 @@ export class HexColorService {
                 console.warn(
                   `Got invalid flag value '${evaluationDetails.value}' for ${hookContext.flagKey}, returning ${hookContext.defaultValue}`
                 );
+                /**
+                 * Throwing an error in the after hook will be caught by the OpenFeature client
+                 * and the default value passed in the `getStringValue` method will be returned.
+                 */
                 throw new Error(
                   `Invalid hex value: ${evaluationDetails.value}`
                 );
