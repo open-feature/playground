9748de9bab1ec7b51c94ad610fb08cb9cb6311b9
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-09-26 21:10:26 -0400

    WIP

---
+++ packages/js-harness-provider/README.md
@@ -0,0 +1,11 @@
+# js-harness-provider
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build js-harness-provider` to build the library.
+
+## Running unit tests
+
+Run `nx test js-harness-provider` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/js-harness-provider/src/lib/js-harness-provider.ts
@@ -0,0 +1,106 @@
+import { Client } from '@harnessio/ff-nodejs-server-sdk';
+import { parseValidJsonObject, parseValidNumber, TypeMismatchError } from '@openfeature/extra';
+import { JsonValue } from '@openfeature/js-sdk';
+import { EvaluationContext, Provider, ResolutionDetails } from '@openfeature/openfeature-js';
+
+/**
+ * NOTE: This is an unofficial provider that was created for demonstration
+ * purposes only. The playground environment will be updated to use official
+ * providers once they're available.
+ */
+export class OpenFeatureSplitProvider implements Provider {
+  metadata = {
+    name: 'harness',
+  };
+  private initialized: Promise<void>;
+  private client: IClient;
+
+  constructor(options: SplitProviderOptions) {
+    this.client = options.splitClient;
+    // we don't expose any init events at the moment (we might later) so for now, lets create a private
+    // promise to await into before we evaluate any flags.
+    this.initialized = new Promise((resolve) => {
+      this.client.on(this.client.Event.SDK_READY, () => {
+        console.log(`${this.metadata.name} provider initialized`);
+        resolve();
+      });
+    });
+  }
+
+  async resolveBooleanEvaluation(
+    flagKey: string,
+    _: boolean,
+    context: EvaluationContext
+  ): Promise<ResolutionDetails<boolean>> {
+    const details = await this.evaluateTreatment(flagKey, this.transformContext(context));
+
+    let value: boolean;
+    switch (details.value as unknown) {
+      case 'on':
+        value = true;
+        break;
+      case 'off':
+        value = false;
+        break;
+      case 'true':
+        value = true;
+        break;
+      case 'false':
+        value = false;
+        break;
+      case true:
+        value = true;
+        break;
+      case false:
+        value = false;
+        break;
+      default:
+        throw new TypeMismatchError(`Invalid boolean value for ${details.value}`);
+    }
+    return { ...details, value };
+  }
+
+  async resolveStringEvaluation(
+    flagKey: string,
+    _: string,
+    context: EvaluationContext
+  ): Promise<ResolutionDetails<string>> {
+    return this.evaluateTreatment(flagKey, this.transformContext(context));
+  }
+
+  async resolveNumberEvaluation(
+    flagKey: string,
+    _: number,
+    context: EvaluationContext
+  ): Promise<ResolutionDetails<number>> {
+    const details = await this.evaluateTreatment(flagKey, this.transformContext(context));
+    return { ...details, value: parseValidNumber(details.value) };
+  }
+
+  async resolveObjectEvaluation<U extends JsonValue>(
+    flagKey: string,
+    _: U,
+    context: EvaluationContext
+  ): Promise<ResolutionDetails<U>> {
+    const details = await this.evaluateTreatment(flagKey, this.transformContext(context));
+    return { ...details, value: parseValidJsonObject(details.value) };
+  }
+
+  private async evaluateTreatment(flagKey: string, consumer: Consumer): Promise<ResolutionDetails<string>> {
+    await this.initialized;
+    const value = this.client.getTreatment(consumer.key, flagKey, consumer.attributes);
+    return { value };
+  }
+
+  //Transform the context into an object useful for the Split API, an key string with arbitrary Split "Attributes".
+  private transformContext(context: EvaluationContext): Consumer {
+    {
+      const { targetingKey, ...attributes } = context;
+      return {
+        key: targetingKey || 'anonymous',
+        // Stringify context objects include date.
+        attributes: JSON.parse(JSON.stringify(attributes)),
+      };
+    }
+  }
+}
