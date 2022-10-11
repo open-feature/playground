266107aaf1828fac276776b9e406721ca77147f0
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-10-11 11:19:20 -0400

    update based on pr feedback
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- packages/fibonacci-service/src/main.ts
+++ packages/fibonacci-service/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 30001;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
+  Logger.log(`🚀 Fibonacci Service is running on: http://localhost:${port}/`);
 }
 
 bootstrap();
