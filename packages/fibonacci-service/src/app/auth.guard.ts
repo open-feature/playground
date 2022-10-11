18eff6b75eb7bce6dbe07f52308cc9cfa0b3e307
Michael Beemer <beeme1mr@users.noreply.github.com>
2022-10-11 15:56:01 -0400

    fix: disable auth in fib service when env var aren't defined (#95)
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- packages/fibonacci-service/src/app/auth.guard.ts
+++ packages/fibonacci-service/src/app/auth.guard.ts
@@ -1,12 +1,22 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly logger = new Logger(AuthGuard.name);
+
+  private readonly FIB_SERVICE_USER = process.env.FIB_SERVICE_USER;
+  private readonly FIB_SERVICE_PASS = process.env.FIB_SERVICE_PASS;
+
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('authorization');
+
+    if (!this.FIB_SERVICE_USER || !this.FIB_SERVICE_PASS) {
+      this.logger.debug("Basic auth hasn't been configured, skipping.");
+      return true;
+    }
 
     if (!authHeader) {
       // Throw 401 response code
@@ -15,7 +25,7 @@ export class AuthGuard implements CanActivate {
     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     const [user, password] = auth;
 
-    if (process.env.FIB_SERVICE_USER === user && process.env.FIB_SERVICE_PASS === password) {
+    if (this.FIB_SERVICE_USER === user && this.FIB_SERVICE_PASS === password) {
       return true;
     }
 
