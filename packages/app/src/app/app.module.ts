c3aa6ca5251099e0aae13b306c8e4cc0303ef778
Lukas Reining <lukas.reining@codecentric.de>
2024-04-15 18:09:35 +0200

    feat: add OFREP and update dependencies
    Signed-off-by: Lukas Reining <lukas.reining@codecentric.de>

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import {HttpModule} from '@nestjs/axios';
-import {ExecutionContext, Module} from '@nestjs/common';
-import {ServeStaticModule} from '@nestjs/serve-static';
-import {LoggingHook, OpenFeatureLogger} from '@openfeature/extra';
-import {TracingHook as SpanEventBasedTracingHook, MetricsHook} from '@openfeature/open-telemetry-hooks';
-import {ProviderService} from '@openfeature/provider';
-import {Request} from 'express';
-import {Agent} from 'http';
-import {LoggerModule} from 'nestjs-pino';
-import {join} from 'path';
-import {FibonacciAsAServiceController} from './fibonacci-as-a-service.controller';
-import {FibonacciService} from './fibonacci/fibonacci.service';
-import {ProvidersController} from './providers.controller';
-import {UtilsController} from './utils.controller';
-import {EvaluationContext, FlagMetadata, OpenFeatureModule} from "@openfeature/nestjs-sdk";
+import { HttpModule } from '@nestjs/axios';
+import { ExecutionContext, Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { LoggingHook, OpenFeatureLogger } from '@openfeature/extra';
+import { MetricsHook, TracingHook as SpanEventBasedTracingHook } from '@openfeature/open-telemetry-hooks';
+import { ProviderService } from '@openfeature/provider';
+import { Request } from 'express';
+import { Agent } from 'http';
+import { LoggerModule } from 'nestjs-pino';
+import { join } from 'path';
+import { FibonacciAsAServiceController } from './fibonacci-as-a-service.controller';
+import { FibonacciService } from './fibonacci/fibonacci.service';
+import { ProvidersController } from './providers.controller';
+import { UtilsController } from './utils.controller';
+import { EvaluationContext, FlagMetadata, OpenFeatureModule } from '@openfeature/nestjs-sdk';
 
 function attributeMapper(flagMetadata: FlagMetadata) {
   return {
-    ...('scope' in flagMetadata && {scope: flagMetadata.scope}),
+    ...('scope' in flagMetadata && { scope: flagMetadata.scope }),
   };
 }
 
@@ -34,11 +34,11 @@ function attributeMapper(flagMetadata: FlagMetadata) {
         transport:
           process.env['NODE' + '_ENV'] !== 'production'
             ? {
-              target: 'pino-pretty',
-              options: {
-                hideObject: true,
-              },
-            }
+                target: 'pino-pretty',
+                options: {
+                  hideObject: true,
+                },
+              }
             : undefined,
       },
     }),
@@ -46,16 +46,20 @@ function attributeMapper(flagMetadata: FlagMetadata) {
       rootPath: join(__dirname, '..', 'ui'),
     }),
     HttpModule.register({
-      httpAgent: new Agent({keepAlive: true}),
+      httpAgent: new Agent({ keepAlive: true }),
     }),
     OpenFeatureModule.forRoot({
       // Set a global logger for OpenFeature. This is logger will available in hooks.
       logger: new OpenFeatureLogger('OpenFeature'),
       //Adding hooks to at the global level will ensure they always run as part of a flag evaluation lifecycle.
-      hooks: [new LoggingHook(), new SpanEventBasedTracingHook({attributeMapper}), new MetricsHook({attributeMapper})],
+      hooks: [
+        new LoggingHook(),
+        new SpanEventBasedTracingHook({ attributeMapper }),
+        new MetricsHook({ attributeMapper }),
+      ],
       // This context will be used for all flag evaluations in the callstack
       contextFactory: async (context: ExecutionContext): Promise<EvaluationContext> => {
-        const req = await context.switchToHttp().getRequest<Request>()
+        const req = await context.switchToHttp().getRequest<Request>();
         const authHeaderValue = req.header('Authorization') || 'anonymous';
         const userAgent = req.header('user-agent');
         return {
@@ -64,17 +68,13 @@ function attributeMapper(flagMetadata: FlagMetadata) {
           email: authHeaderValue,
           method: req.method,
           path: req.path,
-          ...(userAgent && {userAgent}),
+          ...(userAgent && { userAgent }),
           targetingKey: authHeaderValue,
         };
       },
-    })
+    }),
   ],
   controllers: [FibonacciAsAServiceController, UtilsController, ProvidersController],
-  providers: [
-    FibonacciService,
-    ProviderService,
-  ],
+  providers: [FibonacciService, ProviderService],
 })
-export class AppModule {
-}
+export class AppModule {}
