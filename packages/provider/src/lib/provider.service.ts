ffd0abcfe36148b285ed06b077de19d3a7831743
Lukas Reining <lukas.reining@codecentric.de>
2024-04-21 23:49:54 +0200

    fix: use OF provider for GOFF OFREP in the ui
    Signed-off-by: Lukas Reining <lukas.reining@codecentric.de>

--- packages/provider/src/lib/provider.service.ts
+++ packages/provider/src/lib/provider.service.ts
@@ -113,13 +113,14 @@ export class ProviderService {
         new GoFeatureFlagProvider({
           endpoint: process.env.GO_FEATURE_FLAG_URL as string,
         }),
-      available: () => !!process.env.GO_FEATURE_FLAG_WEB_URL,
+      available: () => !!process.env.GO_FEATURE_FLAG_URL,
+      url: process.env.GO_FEATURE_FLAG_WEB_URL as string
     },
     [GO_OFREP_PROVIDER_ID]: {
       factory: () => {
         return new OFREPProvider({ baseUrl: process.env.GO_FEATURE_FLAG_URL as string });
       },
-      available: () => !!process.env.GO_FEATURE_FLAG_URL,
+      available: () => !!process.env.GO_FEATURE_FLAG_WEB_URL,
       url: process.env.GO_FEATURE_FLAG_WEB_URL as string
     },
     [FLAGSMITH_PROVIDER_ID]: {
